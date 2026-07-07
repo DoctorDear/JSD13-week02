34b194e4de2866f3405cecce3ca28c59d2a458f0
Chirasak Tapphae <dearchirasak@gmail.com>
2026-07-07 11:11:36 +0700

    feat: add MongoDB query exercises for movie database filtering and counting

--- mongoDB/ex6.query.mongodb.js
+++ mongoDB/ex6.query.mongodb.js
@@ -1,14 +1,18 @@
 use("sample_mflix")
 
 // Find all movies in the "movies" collection released between 1950 and 1970 (inclusive) that are documented as being shown in countries located in the USA.
-db.movies.find({countries: {$in : ["USA"]
-}, year:{$gte: 1950, $lte: 1970}})
+db.movies.find({
+    countries: {
+        $in: ["USA"]
+    }, year: { $gte: 1950, $lte: 1970 }
+})
 
 
 // What is the number of movies in the "movies" collection with genres "Drama" and "History" released after the year 1970?
-
+db.movies.find({ genres: { $all: ["Drama", "History"] }, year: { $gt: 1970 } }).count()
 
 // In how many films is Roy L. McCardell credited as an actor?
+db.movies.find({ cast: "Roy L. McCardell" }).count()
 
 
 // How many movies did Hal Roach directed?
